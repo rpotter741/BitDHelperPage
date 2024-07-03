d446482f15a1c201f5b3749cc1be10f177034ebd
rpotter741 <robert.potter.741@gmail.com>
2024-07-03 15:14:54 -0400

    Create base app and files

---
+++ asset/data.js
@@ -0,0 +1,10 @@
+var gangData = [
+    {
+        name: 'Gangreen Gang',
+        level: 1,
+        description: ['The gangreen gang sucks ass.', 'Nobody likes them.','Except for your stupid mom.'],
+        notes: '',
+    }
+]
+
+export { gangData }
---
+++ src/app.js
@@ -0,0 +1,11 @@
+import { gangData } from "../asset/data";
+
+$(function() {
+    var $app = $('#app');
+    $app.html('');
+
+    var $header = $('<div class="header"></div>')
+    $header.text('Boi\'s Helper App')
+    $header.appendTo($app);
+
+})
