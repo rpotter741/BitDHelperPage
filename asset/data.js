c29cc7d5ed5047f6c9552d3216373a4197994b66
rpotter741 <robert.potter.741@gmail.com>
2024-07-05 09:57:46 -0400

    Add data, change font size for card details

--- asset/data.js
+++ asset/data.js
@@ -1,6 +1,62 @@
 var gangData = {
-    'Dread Gulch (Global)': [],
-    'Dead Springs': [],
+    'Dread Gulch (Global)': [
+        {
+            name: 'The Sheriffs',
+            leader: 'Unknown',
+            score: 5,
+            tier: 'V',
+            hold: 'S',
+            status: 0,
+            description: [
+                'The Sheriffs of Dread Gulch have a presence in every town in Dread Gulch in the form of a Sheriff-Deputy pair. Nigh impossible to kill, Sheriffs maintain the law and order across Dread Gulch as best they can.',
+                'Sheriffs lack the manpower to tackle criminal organizations at their source, and instead rely on the good citizens of Dread Gulch to report wrong-doings and take small matters into their own hands.'
+            ],
+            notes: '',
+        },
+        {
+            name: 'The Wanderers',
+            leader: 'Motchu Nahawi',
+            score: 4,
+            tier: 'IV',
+            hold: 'W',
+            status: 0,
+            description: [
+                'The Wanderers are a group of nomadic people who live in tribes and have a powerful connection with The Shear, nature, and spirits.',
+                'Simplistic, ritualistic, and considered by some to be barbaric, The Wanderers are slow to trust outsiders but prove to be remarkable allies.'
+            ],
+            notes: '',
+        },
+    ],
+    'Dead Springs': [
+        {
+            name: 'Conductor\'s Union',
+            leader: 'Ole Smokestack',
+            score: 3,
+            tier: 'III',
+            hold: 'S',
+            status: 0,
+            description: [
+                'The Conductor\'s Union represents all of the train conductors across Dread Gulch. They have a strong position in Dead Springs but rely on other factions for the protection of railways across Dread Gulch.',
+                'A lone Conductor is often capable of dealing with a minor gang by themselves - that is to say, Conductors are not to be trifled with.',
+                'It is rumored that the Ghost Train Conductor is a part of the Conductor\'s Union, or that Ole Smokestack himself drives the train.'
+            ],
+            notes: '',
+        },
+        {
+            name: 'Welcoming Committee',
+            leader: 'Mr. Crawshanks',
+            score: 1,
+            tier: 'I',
+            hold: 'W',
+            status: 0,
+            description: [
+                'The Welcoming Committee is responsible for welcoming new-comers to Dread Gulch, handing out information pamphlets (like the one you\'re reading) and generally preventing folks from getting lost upon arrival.',
+                'Were it not for the protection of The Sheriffs, the Welcoming Committee would likely be eradicated and Dead Springs usurped by a stronger faction.',
+                'It is well known, though, that Mr. Crawshanks is not above the occasional kind gesture to sway how arrivals feel about certain groups.'
+            ],
+            notes: '',
+        },
+    ],
     'Skull Crags': [
         {
           name: 'Gangreen Gang',
@@ -8,8 +64,8 @@ var gangData = {
           score: 1,
           tier: 'I',
           hold: 'S',
-          status: 4,
-          description: ['The gangreen gang sucks ass.', 'Nobody likes them.','Except for your stupid mom.'],
+          status: 0,
+          description: ['The Gangreen Gang ', 'Nobody likes them.','Except for your stupid mom.'],
           notes: '',
         },
         {
